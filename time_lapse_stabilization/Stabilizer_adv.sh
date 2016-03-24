40527717104be4efd8a5c46ed7bf3fe57dbd0d97
Steffen_ubuntu <s>
2016-03-24 13:39:48 +0100

    2 channel stabilization as bmp output

--- time_lapse_stabilization/Stabilizer_adv.sh
+++ time_lapse_stabilization/Stabilizer_adv.sh
@@ -4,24 +4,25 @@
  
 begin=$(date +"%s")
  
- 
-folder='./for_Steffi_8bit/'
+#Registration is performed only on Ch1; Ch2 is corrected than accordingly
+folder='./output_ch1/'
+folder2='./output_ch2/'
 #folder='./Cells/'
-ext_in=tif
-ext_out=mha
+ext_in=tiff
+ext_out=bmp
 parameters_file=Parameters_Rigid.txt
  
- 
- 
- 
+
 i=1
 j=2
 files=(ls $folder*)
+#files=(ls -v $folder*|sort -V)
+files2=(ls $folder2*)
 echo "${files[$i]}"
 echo "${files[$j]}"
  
 count=$(ls $folder | wc -l)
-#count=2
+#count=10
 echo $count
  
 while [ $i -lt $count ]; do
@@ -39,6 +40,8 @@ l=2
 while [ $i -lt $count ]; do
         echo item: ${files[$i]} plus ${files[$j]}
         sed -i '4s/.*/(InitialTransformParametersFileName ".\/outdir'$k$l'\/TransformParameters.0.txt")/' ./outdir$i$j/TransformParameters.0.txt 
+        sed -i '29s/.*/(ResultImageFormat "bmp")/' ./outdir$i$j/TransformParameters.0.txt 
+        sed -i '30s/.*/(ResultImagePixelType "unsigned char")/' ./outdir$i$j/TransformParameters.0.txt 
         let i=i+1
         let j=j+1
         let k=k+1
@@ -49,12 +52,24 @@ while [ $i -lt $count ]; do
 i=1
 j=2
 mkdir ./results/
+mkdir ./results/ch1
+mkdir ./results/ch2
 while [ $i -lt $count ]; do
         #echo item: ${files[$i]} plus ${files[$j]}
-        transformix -in ${files[$j]} -tp ./outdir$i$j/TransformParameters.0.txt -out ./results/ -threads 4 -def all
+        #transformix -in ${files[$j]} -tp ./outdir$i$j/TransformParameters.0.txt -out ./results/ -threads 4 -def all
+        transformix -in ${files[$j]} -tp ./outdir$i$j/TransformParameters.0.txt -out ./results/ch1/ -threads 4 
         s=${files[$j]##*/}
+        mv ./results/ch1/result.$ext_out ./results/ch1/${s%.$ext_in}.$ext_out
+        transformix -in ${files2[$j]} -tp ./outdir$i$j/TransformParameters.0.txt -out ./results/ch2/ -threads 4 
+        t=${files2[$j]##*/}
+        mv ./results/ch2/result.$ext_out ./results/ch2/${t%.$ext_in}.$ext_out
+        #mv ./results/deformationField.mhd ./results/deformationField/deformationField_${s%.$ext_in}.mhd
+        #sed -i '14s/.*/(ElementDataFile = deformationField_'${s%.$ext_in}'.raw/' ./results/deformationField/deformationField_${s%.$ext_in}.mhd
+        #mv ./results/deformationField.raw ./results/deformationField/deformationField_${s%.$ext_in}.raw
+        #transformix -in ${files2[$j]} -tp ./outdir$i$j/TransformParameters.0.txt -out ./results/ -threads 4 -def all
+        #t=${files2[$j]##*/}
         #mv ./results/result.$ext_out ./results/${s%.$ext_in}.$ext_out
-        mv ./results/deformationField.$ext_out ./results/deformationField_${s%.$ext_in}.$ext_out
+        #mv ./results/deformationField.$ext_out ./results/deformationField_${t%.$ext_in}.$ext_out
         let i=i+1
         let j=j+1
         done   
